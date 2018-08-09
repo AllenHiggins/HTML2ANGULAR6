f6f132b2f4f56f03bd2e52ea6d93a63636b6287e
AllenHiggins <allenghiggins@gmail.com>
2018-08-09 12:40:38 +0100

    aerial completed

--- src/app/aerial/aerial.component.ts
+++ src/app/aerial/aerial.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aerial.component.scss']
 })
 export class AerialComponent implements OnInit {
-
+  title: string;
+  subTitle: string;
+  links = [];
   constructor() { }
 
   ngOnInit() {
+    this.title = 'Adam Jensen';
+    this.subTitle = 'Security Chief . Cyborg . Never asked for this';
+    this.links = [
+      'fa fa-twitter',
+      'fa fa-facebook-f',
+      'fa fa-dribbble',
+      'fa fa-github',
+      'fa fa-envelope-o'
+    ];
   }
 
 }
